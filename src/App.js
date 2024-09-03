4beadb540b06db9dc80b766009691c6d66a2b6a3
Yogamahi <mahiyoga013@gmail.com>
2024-09-03 15:35:35 +0530

    Final commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import './App.css';
 import Header from "./components/Header"
+import PageNotFound from './components/PageNotFound';
 import WordCounter from './components/WordCounter';
+import { Route,Routes } from 'react-router-dom';
 function App() {
   return (
     <div className="App">
       <Header />
-      <WordCounter />
+      <Routes>
+        <Route path='/' element={<WordCounter />} />
+        <Route path='*' element={<PageNotFound />} />
+      </Routes>     
     </div>
   );
 }
---
+++ src/components/PageNotFound.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const PageNotFound = () => {
+  return (
+    <div className='error-page'>
+        <h1>Oops...! Page Not Found</h1>
+    </div>
+  )
+}
+
+export default PageNotFound;
